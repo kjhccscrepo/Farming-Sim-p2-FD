f1b126b969f8e1fbce5a47355e2063390c064542
kjhccscrepo <j_kist1@mail.harpercollege.edu>
2025-11-20 20:52:46 -0600

    minor syntax changes

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 
 #include "logic/cupid.hpp"
 
-// to-do list
-// fix shop, as its currently semi-functioning.
-// add tests for all the test files in farms_test
-
-
 int main() {
     cupid game;
     game.runGame();
